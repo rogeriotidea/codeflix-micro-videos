71d567b79aa35de0a2b149f630fe518859300687
Rogerio Coelho <suporte@tidea.com.br>
2022-03-11 08:23:21 -0300

    first commit

---
+++ category.ts
@@ -0,0 +1,11 @@
+
+
+class Category {
+    private id: string;
+    private name: string;
+    private description: string;
+    private is_active: string;
+    private created_at: Date;
+
+
+}
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+"use strict";
+class Test {
+    private a: string = 'a'
+}
